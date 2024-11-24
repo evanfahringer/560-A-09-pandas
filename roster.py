767d6fa64fe5a9d86f09c27b0f5410318194f8c1
Evan Fahringer <fahringerevan@gmail.com>
2024-11-24 15:46:44 -0500

    0 Header

--- roster.py
+++ roster.py
@@ -1,5 +1,9 @@
 # https://goheels.com/sports/mens-basketball/roster
+import pandas as pd
 
 roster = ["Bacot", "Davis", "Cadeau"]
+data = pd.DataFrame(roster)
+print(data)
+
 for player in roster:
    print(player)
