995c66537b6de40af63c5a649554b661968e5002
Evan Fahringer <fahringerevan@gmail.com>
2024-11-24 15:50:07 -0500

    Last Name Header

--- roster.py
+++ roster.py
@@ -2,7 +2,8 @@
 import pandas as pd
 
 roster = ["Bacot", "Davis", "Cadeau"]
-data = pd.DataFrame(roster)
+player = {"Last Name": roster}
+data = pd.DataFrame(player)
 print(data)
 
 for player in roster:
